028fc0d9a2692594d7547a112aa8a5a09d898cb7
Gloomsfield <amory@acosta.boo>
2026-06-28 12:55:16 -0700

    add stub definitions to parse.c

---
+++ src/parse/parse.c
@@ -0,0 +1,7 @@
+#include "parse.h"
+
+#include "../types/types.h"
+
+int parse_text(int argc, char* argv[], request_data_t* request_data) {}
+
+int parse_devices(int argc, char* argv[], request_data_t* request_data) {}
