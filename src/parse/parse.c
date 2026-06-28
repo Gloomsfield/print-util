814024659b9e4f85434291a4bd0f0c4c9aaab7ca
Gloomsfield <amory@acosta.boo>
2026-06-28 12:55:50 -0700

    fix returns in stub definitions

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -2,6 +2,6 @@
 
 #include "../types/types.h"
 
-int parse_text(int argc, char* argv[], request_data_t* request_data) {}
+int parse_text(int argc, char* argv[], request_data_t* request_data) { return -1; }
 
-int parse_devices(int argc, char* argv[], request_data_t* request_data) {}
+int parse_devices(int argc, char* argv[], request_data_t* request_data) { return -1; }
