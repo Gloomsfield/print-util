2929f6013dd7c7e1d058ec59ac0a7bb10b54b707
Gloomsfield <amory@acosta.boo>
2026-06-28 18:04:56 -0700

    add status.h

---
+++ src/status.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stdio.h>
+
+#define STATUSES(CHOOSE_FUNC) \
+	CHOOSE_FUNC(FAILURE, "generic failure!", -1) \
+	CHOOSE_FUNC(SUCCESS, "success!", 0)
+
+#define CHOOSE_ENUM(ENUM, STRING, EXIT_CODE) ENUM,
+#define CHOOSE_STRING(ENUM, STRING, EXIT_CODE) STRING,
+#define CHOOSE_EXIT_CODE(ENUM, STRING, EXIT_CODE) EXIT_CODE,
+
+enum status_t { STATUSES(CHOOSE_ENUM) } status_t;
+static char* status_strings[] = { STATUSES(CHOOSE_STRING) };
+static int status_exit_codes[] = { STATUSES(CHOOSE_EXIT_CODE) };
+
+#define ASSERT(condition, cleanup, state, fail_status) { \
+	if(!(condition)) { \
+		cleanup((state)); \
+		fprintf(stderr, "print-util error: %s\n", status_strings[(fail_status)]); \
+	} \
+}
